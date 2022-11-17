0ba751351ab67102db4f8447e506a14e7a333581
Хромченков Александр Денисович <ADeKhromchenkov@sberbank.ru>
2022-11-17 18:43:37 +0300

    test

---
+++ Hello.sol
@@ -0,0 +1,18 @@
+
+pragma solidity ^0.8.9;
+
+contract HelloWorld {
+  string public greet = 'Hello world';
+  address public sender;
+  uint public amount;
+
+  function pay() external payable {
+    sender = msg.sender;
+    amount = msg.value;
+  }
+
+  function fail() external {
+    require(false, 'FAIled here');
+    greet = '';
+  }
+}
